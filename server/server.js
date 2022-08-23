d51d3798006946d3b02bdcd83a27f001ba30e43b
Omar Mabrouk <67244800+SpadeQ22@users.noreply.github.com>
2022-08-24 01:15:31 +0200

    finished backend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,35 @@
 require('dotenv').config();
-const path = require("path")
-const express= require('express')
-const app = express()
+const path = require("path");
+const express= require('express');
+const app = express();
+const { Pool } = require('pg');
+
 app.use(express.json())
+    
+const clientPool = new Pool({
+    host: 'database-1.cvwemstkxffy.us-east-1.rds.amazonaws.com',
+    user: 'postgres',
+    database: 'NFT_MarketPlace',
+    password: 'adminadmin',
+    port: 5432,
+});
 
 const PORT = process.env.PORT || 3001;
+
+
+AWS.config.update({
+  accessKeyId: process.env.Access_Key_ID,
+  secretAccessKey: Secret_Access_Key
+});
+
+var s3 = new AWS.S3();
+
+
+var params = {
+  Bucket: "blockchain-training",
+  Body: "filestream",
+  key:"file-name"
+};
 
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -13,33 +38,65 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 })
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Welcome from Server!!"});
-})
+
+
+app.get("/userinfo/:address", (req, res) => {
+    let address = req.params.address;
+    clientPool.connect((err, client)=>{
+      if(err){
+        console.log(err.stack);
+      } else {
+        client.query("SELECT * FROM client WHERE address = '" + address + "';", (err, result) =>{
+          if(err){
+            console.log(err.stack);
+          }else {
+            res.send(result['rows']['0']);
+          }
+        });
+        client.release();
+      }
+    });
+});
+
+
+app.get("/nftinfo/:address", (req, res) => {
+  let address = req.params.address;
+  clientPool.connect((err, client)=>{
+    if(err){
+      console.log(err.stack);
+    } else {
+      client.query("SELECT * FROM nft_object WHERE ownerAddress = '" + address + "';", (err, result) =>{
+        if(err){
+          console.log(err.stack);
+        }else {
+          res.send(result['rows']);
+        }
+      });
+      client.release();
+    }
+  });
+});
+
+
+app.get("/nftinfo", (req, res) => {
+  let address = req.params.address;
+  clientPool.connect((err, client)=>{
+    if(err){
+      console.log(err.stack);
+    } else {
+      client.query("SELECT * FROM nft_object;", (err, result) =>{
+        if(err){
+          console.log(err.stack);
+        }else {
+          res.send(result['rows']);
+        }
+      });
+      client.release();
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-
-// var pg = require('pg');
-// var connectionString = "postgres://postgres:adminadmin@database-1.cvwemstkxffy.us-east-1.rds.amazonaws.com:5432/NFT_MarketPlace";
-
-// var client = new pg.Client(connectionString);
-
-// client.connect(function(err) {
-//   if (err) {
-//     console.error('Database connection failed: ' + err.stack);
-//     return;
-//   }
-
-//   console.log('Connected to database.');
-// });
-
-// client.end();
-
-var client = require('./client');
-
-var t = client.getClientInfo('0xA1f61C8CA661B198971697279a56D15d9F1B3329');
-
-console.log(t);
