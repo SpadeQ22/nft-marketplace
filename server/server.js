6638b6d0ded6c4d94121dc0cfccec10e148fc077
Omar Mabrouk <67244800+SpadeQ22@users.noreply.github.com>
2022-08-25 02:00:35 +0200

    added upload path and user registeration path

--- server/server.js
+++ server/server.js
@@ -1,20 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 const path = require("path");
 const express= require('express');
 const app = express();
-const { Pool } = require('pg');
+const {Pool} = require("pg");
+const Busboy = require("busboy");
 
-app.use(express.json())
-    
+app.use(express.json());
+
 const clientPool = new Pool({
-    host: 'database-1.cvwemstkxffy.us-east-1.rds.amazonaws.com',
-    user: 'postgres',
-    database: 'NFT_MarketPlace',
-    password: 'adminadmin',
-    port: 5432,
+  host: "database-1.cvwemstkxffy.us-east-1.rds.amazonaws.com",
+  user: "postgres",
+  database: "NFT_MarketPlace",
+  password: "adminadmin",
+  port: 5432,
 });
 
 const PORT = process.env.PORT || 3001;
+
+
+AWS.config.update({
+  accessKeyId: process.env.Access_Key_ID,
+  secretAccessKey: Secret_Access_Key
+});
+
+var s3 = new AWS.S3();
+
 
 var params = {
   Bucket: "blockchain-training",
@@ -26,60 +36,62 @@ var params = {
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
-
-
-
-app.get("/userinfo/:address", (req, res) => {
-    let address = req.params.address;
-    clientPool.connect((err, client)=>{
-      if(err){
-        console.log(err.stack);
-      } else {
-        client.query("SELECT * FROM client WHERE address = '" + address + "';", (err, result) =>{
-          if(err){
-            console.log(err.stack);
-          }else {
-            res.send(result['rows']['0']);
-          }
-        });
-        client.release();
-      }
-    });
+  res.send("Hello World!");
 });
 
+app.get("/userinfo/:address", (req, res) => {
+  let address = req.params.address;
+  clientPool.connect((err, client) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      client.query(
+        "SELECT * FROM client WHERE address = '" + address + "';",
+        (err, result) => {
+          if (err) {
+            console.log(err.stack);
+          } else {
+            res.send(result["rows"]["0"]);
+          }
+        }
+      );
+      client.release();
+    }
+  });
+});
 
 app.get("/nftinfo/:address", (req, res) => {
   let address = req.params.address;
-  clientPool.connect((err, client)=>{
-    if(err){
+  clientPool.connect((err, client) => {
+    if (err) {
       console.log(err.stack);
     } else {
-      client.query("SELECT * FROM nft_object WHERE ownerAddress = '" + address + "';", (err, result) =>{
-        if(err){
-          console.log(err.stack);
-        }else {
-          res.send(result['rows']);
+      client.query(
+        "SELECT * FROM nft_object WHERE ownerAddress = '" + address + "';",
+        (err, result) => {
+          if (err) {
+            console.log(err.stack);
+          } else {
+            res.send(result["rows"]);
+          }
         }
-      });
+      );
       client.release();
     }
   });
 });
-
 
 app.get("/nftinfo", (req, res) => {
   let address = req.params.address;
-  clientPool.connect((err, client)=>{
-    if(err){
+  clientPool.connect((err, client) => {
+    if (err) {
       console.log(err.stack);
     } else {
-      client.query("SELECT * FROM nft_object;", (err, result) =>{
-        if(err){
+      client.query("SELECT * FROM nft_object;", (err, result) => {
+        if (err) {
           console.log(err.stack);
-        }else {
-          res.send(result['rows']);
+        } else {
+          res.send(result["rows"]);
         }
       });
       client.release();
@@ -87,12 +99,44 @@ app.get("/nftinfo", (req, res) => {
   });
 });
 
-app.post("/userinfo", (req, res) => {
-  console.log(req.body);
+app.post("/userinfo", (req, res)=>{
+  clientPool.connect((err, client) =>{
+    if(err){
+      console.log(err.stack);
+      res.send({"message":"couldn't connect to database"});
+    }else {
+      client.query(`INSERT INTO client (name, age, address, phonenumber, email) 
+      VALUES(${req.body.name}, ${req.body.age}, ${req.body.address}, 
+        ${req.body.phonenumber}, ${req.body.email});`, (err, result)=>{
+          if(err){
+            console.log(err.stack);
+            res.send({"message":"error during added"});
+          } else {
+            res.send({"message":"success"});
+          }
+      });
+    }
+  });
 });
+
+
+app.post("/upload", (req, res)=>{
+  const bb = Busboy({headers: req.headers});
+
+  bb.on('file', async(fieldname, file, filename, encoding, mimetype) => {
+    const params = {
+      Bucket: "blockchain-training",
+      Body: file,
+      key:filename
+    }
+    const upload = await s3.upload(params).promise();
+    res.send({"link": upload.Location});
+  })
+
+})
+
 
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
